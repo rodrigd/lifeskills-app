bf689de089007cc5c9103f4b6525b1466ac08e1b
David Rodriguez <dwdrodri@aol.com>
2016-05-15 17:56:18 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lifeskills::Application.config.secret_key_base = '91cf84fcc5c535d601e6cb3f4816b7d4a3eee388e97e31272fce622f858e4e0a386ad039267d736f3dc7c438bbb1be41e0b41b495ffd134aabf1596f1a1e1d86'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lifeskills::Application.config.session_store :cookie_store, key: '_lifeskills_session'
